ece54c8a47e33396afc61daaa506d204c835b6b9
Nathan <31359612+neerb@users.noreply.github.com>
2019-08-14 15:15:51 -0500

    Add files via upload

---
+++ TicTacToe Exercise/TicTacToe Exercise/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TicTacToeGame game = new TicTacToeGame();
+            int decision = 0;
+
+            while(decision != -1)
+            {
+                Console.WriteLine("Welcome to Tic-Tac-Toe!");
+                Console.WriteLine("0 - Start a new game.");
+                Console.WriteLine("-1 - Exit the program.");
+                Console.WriteLine("Select a menu option: ");
+
+                decision = Convert.ToInt32(Console.ReadLine());
+
+                switch(decision)
+                {
+                    case 0:
+                        while (!game.checkForWin() && !game.checkForTie())
+                        {
+                            game.printGameBoard();
+
+                            int x, y;
+
+                            if (game.isXTurn())
+                            {
+                                Console.WriteLine("X Player, enter your coordinates...");
+
+                                Console.Write("Enter X coordinate: ");
+                                x = Convert.ToInt32(Console.ReadLine());
+
+                                Console.Write("Enter Y coordinate: ");
+                                y = Convert.ToInt32(Console.ReadLine());
+
+                                while (!game.makeMove(game.isXTurn(), x, y))
+                                {
+                                    Console.WriteLine("Invalid move: please try again: ");
+
+                                    Console.Write("Enter X coordinate: ");
+                                    x = Convert.ToInt32(Console.ReadLine());
+
+                                    Console.Write("Enter Y coordinate: ");
+                                    y = Convert.ToInt32(Console.ReadLine());
+                                }
+                            }
+                            else
+                            {
+                                Console.WriteLine("O Player, enter your coordinates (x, y):");
+
+                                Console.Write("Enter X coordinate: ");
+                                x = Convert.ToInt32(Console.ReadLine());
+
+                                Console.Write("Enter Y coordinate: ");
+                                y = Convert.ToInt32(Console.ReadLine());
+
+                                while (!game.makeMove(game.isXTurn(), x, y))
+                                {
+                                    Console.WriteLine("Invalid move: please try again: ");
+
+                                    Console.Write("Enter X coordinate: ");
+                                    x = Convert.ToInt32(Console.ReadLine());
+
+                                    Console.Write("Enter Y coordinate: ");
+                                    y = Convert.ToInt32(Console.ReadLine());
+                                }
+                            }
+                        }
+
+                        game.printGameBoard();
+
+                        if (game.checkForTie())
+                            Console.WriteLine("It's a tie!");
+                        
+                        if(game.checkForWin())
+                        {
+                            //Inverted because it defaults to next player
+                            if (game.isXTurn())
+                                Console.WriteLine("O is the winner!");
+                            else
+                                Console.WriteLine("X is the winner!");
+                        }
+
+                        game.reset();
+                        break;
+                    case 1:
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TicTacToe Exercise/TicTacToe Exercise/TicTacToeGame.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe_Exercise
+{
+    class TicTacToeGame
+    {
+        private int sizeSquared = 3;
+        private char[,] gameBoard;
+        private bool xTurn = true;
+
+        public TicTacToeGame()
+        {
+            gameBoard = new char[sizeSquared, sizeSquared];
+
+            for(int x = 0; x < sizeSquared; x++)
+            {
+                for(int y = 0; y < sizeSquared; y++)
+                {
+                    gameBoard[x, y] = ' ';
+                }
+            }
+        }
+
+        public bool isXTurn()
+        {
+            return xTurn;
+        }
+
+        public bool makeMove(bool isX, int x, int y)
+        {
+            bool returnVal = true;
+
+            if (gameBoard[x, y] == ' ')
+            {
+                if (isX)
+                    gameBoard[x, y] = 'X';
+                else
+                    gameBoard[x, y] = 'O';
+
+                xTurn = !xTurn;
+            }
+            else
+                returnVal = false;
+
+            return returnVal;
+        }
+
+        public bool checkForWin()
+        {
+            bool returnVal = false;
+
+            //Check for horizontal wins
+            for(int y = 0; y < sizeSquared; y++)
+            {
+                if (gameBoard[0, y] == gameBoard[1, y] && gameBoard[0, y] == gameBoard[2, y] && gameBoard[0, y] != ' ')
+                    returnVal = true;
+            }
+
+            //Check for vertical wins
+            for (int x = 0; x < sizeSquared; x++)
+            {
+                if (gameBoard[x, 0] == gameBoard[x, 1] && gameBoard[x, 0] == gameBoard[x, 2] && gameBoard[x, 0] != ' ')
+                    returnVal = true;
+            }
+
+            //Diagonal negative wins
+            if (gameBoard[0, 0] == gameBoard[1, 1] && gameBoard[0, 0] == gameBoard[2, 2] && gameBoard[0, 0] != ' ')
+                returnVal = true;
+
+            //Diagonal positive wins
+            if (gameBoard[0, 2] == gameBoard[1, 1] && gameBoard[0, 2] == gameBoard[2, 0] && gameBoard[0, 2] != ' ')
+                returnVal = true;
+
+            return returnVal;
+        }
+
+        public bool checkForTie()
+        {
+            for(int y = 0; y < sizeSquared; y++)
+            {
+                for(int x = 0; x < sizeSquared; x++)
+                {
+                    if(gameBoard[x, y] == ' ')
+                        return false;
+                }
+            }
+
+            return true;
+        }
+
+        public void reset()
+        {
+            for (int x = 0; x < sizeSquared; x++)
+            {
+                for (int y = 0; y < sizeSquared; y++)
+                {
+                    gameBoard[x, y] = ' ';
+                }
+            }
+        }
+
+
+        public void printGameBoard()
+        {
+            Console.WriteLine("Current Game Board:");
+            Console.WriteLine("   -0---1---2-X");
+            for(int y = 0; y < sizeSquared; y++)
+            {
+                Console.Write(y);
+
+                for(int x = 0; x < sizeSquared; x++)
+                {
+                    Console.Write(" | ");
+                    Console.Write(gameBoard[x, y]);
+                }
+                Console.Write(" |");
+
+                Console.WriteLine();
+
+                if(y != sizeSquared - 1)
+                    Console.WriteLine("  |-----------|");
+                else
+                    Console.WriteLine("Y  -----------");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
